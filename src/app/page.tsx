a7bfdf38bc0f3a6c1bb3e113805ed386bd14c0a0
Liz Martins <elizabeth.martins5@gmail.com>
2024-07-04 22:18:44 -0400

    initial form setup with input field for name and button to fetch 'prediction'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+"use client"
+import { useState } from "react";
 
 export default function Home() {
+  const [inputVal, setInputVal] = useState("")
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      NEXT
-    </main>
+<div>
+  <div>
+<h1>Enter Your Name</h1>
+  </div>
+  <form>
+    <input type="text" placeholder="Type your name..." />
+    <button type="submit" >Predict</button>
+  </form>
+</div>
   );
 }
