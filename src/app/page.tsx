ddc5b04aa7fdd357ed8934cd9dfd6cb1ae6935db
Liz Martins <elizabeth.martins5@gmail.com>
2024-07-04 22:49:34 -0400

    created prediction folder and routing functionality to go from pressing Predict button to displaying prediction page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
 "use client"
-import { useState } from "react";
+import { useState, FormEvent } from "react";
+import {useRouter} from 'next/navigation'
 
 export default function Home() {
   const [inputVal, setInputVal] = useState("")
+  const {push} = useRouter()
+  const handleSubmit = (event:FormEvent) => {
+    event.preventDefault();
+    push(`/prediction/${inputVal}`)
+  }
   return (
 <div>
   <div>
 <h1>Enter Your Name</h1>
   </div>
-  <form>
-    <input type="text" placeholder="Type your name..." />
+  <form onSubmit={handleSubmit}>
+    <input className="text-black" type="text" placeholder="Type your name..." value={inputVal} onChange={(e) => setInputVal(e.target.value)} />
     <button type="submit" >Predict</button>
   </form>
 </div>
---
+++ src/app/prediction/[name]/page.tsx
@@ -0,0 +1,11 @@
+interface Params {
+  params: {name: string}
+}
+
+export default function Page({params}: Params) {
+  return (
+    <main>
+      {params.name}
+    </main>
+  )
+}
