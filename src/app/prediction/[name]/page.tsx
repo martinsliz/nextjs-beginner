34340ce60b4aa27073f9c3a70c0c9b5fe07ff132
Liz Martins <elizabeth.martins5@gmail.com>
2024-07-05 20:05:11 -0400

    added api calls for each prediction

--- src/app/prediction/[name]/page.tsx
+++ src/app/prediction/[name]/page.tsx
@@ -1,10 +1,29 @@
+const getPredictedAge = async (name: string) => {
+  const res = await fetch(`https://api.agify.io/?name=${name}`)
+  return res.json()
+}
+
+const getPredictedGender = async (name: string) => {
+  const res = await fetch(`https://api.genderize.io/?name=${name}`)
+  return res.json()
+}
+const getPredictedCountry = async (name: string) => {
+  const res = await fetch(`https://api.nationalize.io/?name=${name}`)
+  return res.json()
+}
+
 interface Params {
   params: {name: string}
 }
 
-export default function Page({params}: Params) {
+export default async function Page({params}: Params) {
+  const ageData = getPredictedAge(params.name)
+  const genderData = getPredictedGender(params.name)
+  const countryData = getPredictedCountry(params.name)
+
   return (
-    <main>
+    <main className="flex min-h-screen flex-col items-center">
+      {" "}
       {params.name}
     </main>
   )
